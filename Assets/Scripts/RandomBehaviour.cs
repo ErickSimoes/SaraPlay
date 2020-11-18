654f42571e5ea4fa9475d88639c1fb44aa4d003c
Erick Simões <erick.simoes@live.com>
2020-11-18 16:57:19 -0300

    add: Implements RandomBehaviour script

---
+++ Assets/Scripts/RandomBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class RandomBehaviour : MonoBehaviour {
+    [SerializeField]
+    private string[] names;
+    [SerializeField]
+    private TextMeshProUGUI text;
+    private int index = 0;
+
+    void Start() {
+        Shuffle();
+    }
+
+    public void StartSortName() {
+        StartCoroutine(ChooseName());
+    }
+
+    private IEnumerator ChooseName() {
+        int max = 10;
+
+        if (names.Length < 10) {
+            max = name.Length - 1;
+        }
+
+        for (int i = 0; i < max; i++) {
+            text.text = names[i];
+            yield return new WaitForSeconds(.15f);
+        }
+
+        text.text = names[index++];
+
+        if (index >= names.Length) {
+            index = 0;
+        }
+    }
+
+    private void Shuffle() {
+        int rdm;
+        for (int i = 0; i < names.Length; i++) {
+            rdm = Random.Range(0, names.Length - 1);
+            string temp = names[rdm];
+            names[rdm] = names[i];
+            names[i] = temp;
+        }
+    }
+}
