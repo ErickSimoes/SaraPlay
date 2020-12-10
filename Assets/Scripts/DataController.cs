0537a6715aa6db56fbc6720c245b914c6e006319
Erick Simões <erick.simoes@live.com>
2020-12-10 17:49:04 -0300

    Add Sava to file functions

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using TMPro;
+using UnityEngine;
+
+public class DataController : MonoBehaviour {
+
+    private const string fileName = "names_list.json";
+
+    public void SaveTextBox(TextMeshProUGUI textBox) {
+        string namesListJson = JsonHelper.ToJson(textBox.text.Split('\n'), true);
+        File.WriteAllText(Path.Combine(Application.persistentDataPath, fileName), namesListJson);
+    }
+}
