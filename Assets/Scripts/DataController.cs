4075805b011057d630594c0a7d8aa7f5ee465473
Erick Simões <erick.simoes@live.com>
2020-12-13 10:02:41 -0300

    Implements read data method

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -1,13 +1,31 @@
 ﻿using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DataController : MonoBehaviour {
 
     private const string fileName = "names_list.json";
+    [SerializeField] private TMP_InputField textBox;
 
     public void SaveTextBox(TextMeshProUGUI textBox) {
         string namesListJson = JsonHelper.ToJson(textBox.text.Split('\n'), true);
         File.WriteAllText(Path.Combine(Application.persistentDataPath, fileName), namesListJson);
+    }
+
+    public static string[] Read2TextBox() {
+        string allText = File.ReadAllText(Path.Combine(Application.persistentDataPath, fileName));
+        return JsonHelper.FromJson<string>(allText);
+    }
+
+    private void OnEnable() {
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode) {
+        if (textBox) {
+            string[] name_list = Read2TextBox();
+            textBox.text = string.Join("\n", name_list);
+        }
     }
 }
