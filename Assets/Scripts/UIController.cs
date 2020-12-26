08f3022e91b927162483e5c7d0aed312304fda13
Erick Simões <erick.simoes@live.com>
2020-12-26 18:34:55 -0300

    Keep the screen on in GameScene
    Resolves: #8

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
 
+    private void Start() {
+        if (SceneManager.GetActiveScene().name == "GameScene") {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        } else {
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+    }
+
     public void LoadScene(string sceneName) {
         SceneManager.LoadSceneAsync(sceneName);
     }
