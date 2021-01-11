aba8ed6f81d1d366d4024054a496677ec34efcd1
Erick Simões <erick.simoes@live.com>
2021-01-11 14:44:30 -0300

    Add and save the new name

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -3,19 +3,25 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class DataController : MonoBehaviour {
 
     private const string fileName = "names_list.json";
 
-    [SerializeField] private string[] nameList;
+    [SerializeField] private new List<string> nameList;
     [SerializeField] private GameObject panel;
     [SerializeField] private GameObject nameButtonPrefab;
     [SerializeField] private DeleteOrEditController deleteOrEditPanel;
 
-    private void SaveTextBox(TextMeshProUGUI textBox) {
-        string namesListJson = JsonHelper.ToJson(textBox.text.Split('\n'), true);
+    private void SaveNameList() {
+        string namesListJson = JsonHelper.ToJson(nameList.ToArray(), true);
         File.WriteAllText(Path.Combine(Application.persistentDataPath, fileName), namesListJson);
+    }
+    
+    public void SaveName(Text name) {
+        nameList.Add(name.text);
+        SaveNameList();
     }
 
     public static string[] Read2TextBox() {
@@ -28,7 +34,7 @@ public class DataController : MonoBehaviour {
     }
 
     private void OnSceneLoad(Scene scene, LoadSceneMode mode) {
-        nameList = Read2TextBox();
+        nameList.AddRange(Read2TextBox());
         GameObject tempNameButton;
         foreach (string name in nameList) {
             tempNameButton = Instantiate(nameButtonPrefab, panel.transform);
