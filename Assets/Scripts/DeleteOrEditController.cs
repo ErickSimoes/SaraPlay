55bcfc537af2b6c9499a3b4d28d6b757ac5a5d27
Erick Simões <erick.simoes@live.com>
2021-01-12 08:39:57 -0300

    Add remove name function

--- Assets/Scripts/DeleteOrEditController.cs
+++ Assets/Scripts/DeleteOrEditController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DeleteOrEditController : MonoBehaviour {
-    [SerializeField]
-    private Text text;
+    [SerializeField] private Text text;
+    [SerializeField] private DataController dataController;
     
     public void setName(string name) {
         this.gameObject.SetActive(true);
         text.text = name;
+    }
+
+    public void DeleteName() {
+        dataController.RemoveName(text.text);
     }
 }
